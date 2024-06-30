dbde8034e79244235f10a326d07253666abede66
memeryliu <memeryliu@163.com>
2024-06-30 17:21:22 +0800

    #create guess number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
 fn main() {
-    println!("Hello, git!");
+    println!("Guess the number!");
+    println!("please input your guess!");
+
+    let mut guess = String::new();
+
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Fail to read line!");
+
+    println!("your input number is {}", guess);
 }
